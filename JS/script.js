8e01f33aa7d0ac00b404b4d3a6842ec657b5a5f4
Ivan <ivan.s.palatov@gmail.com>
2020-04-16 18:57:36 +0400

    corrected work with array

--- JS/script.js
+++ JS/script.js
@@ -4,25 +4,20 @@ var appData = {
     budget: money,
     timeData: time,
     expenses: {},
-    // optionalExpenses,
-    incom: [],
+    optionalExpenses: {},
+    income: [],
     saving: false,
 };
 
 var answ1 = prompt('Введите обязательную статью расходов в этом месяце', 'qqq');
 var answ2 = +prompt('Во сколько обойдется?', '1000');
-
 var answ3 = prompt('Введите обязательную статью расходов в этом месяце', 'www');
 var answ4 = +prompt('Во сколько обойдется?', '500');
 
-appData.expenses.answ1 = answ1;
-appData.expenses.answ2 = answ2;
-appData.expenses.answ3 = answ3;
-appData.expenses.answ4 = answ4;
-appData.budget = money - answ2 - answ4;
+appData.expenses.answ1 = answ2;
+appData.expenses.answ3 = answ4;
 
-var budgetOneDay = appData.budget / 30;
-console.log(budgetOneDay);
-alert('Ваш бюджет на 1 день в ближайший месяц составит:'+ budgetOneDay);
+console.log(appData.budget / 30);
+alert('Ваш бюджет на 1 день в ближайший месяц составит:'+ appData.budget / 30);
 
 
